80d048ddb0e20287e882cf56c9c4015ef00d7d6a
Nachiketa <m23csa016@iitj.ac.in>
2024-03-30 12:24:18 +0530

    Fixed the bug of tanh function

--- print_activation.py
+++ print_activation.py
@@ -5,4 +5,6 @@ input_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 print(f"Sigmoid Values: {[sigmoid_func(x) for x in input_values]}")
 print(f"ReLU Values: {[relu_func(x) for x in input_values]}")
 print(f"Leaky ReLU Values: {[leaky_relu(x) for x in input_values]}")
-print(f"Tanh Values: {[leaky_relu(x) for x in input_values]}")
+print(f"Tanh Values: {[tanh_func(x) for x in input_values]}")
+
+# Bug of wrong tanh function 
