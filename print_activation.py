1606a9e22604b6f713a80e91a412abb611be151d
Nachiketa <m23csa016@iitj.ac.in>
2024-03-30 12:23:08 +0530

    Added other printing activation

--- print_activation.py
+++ print_activation.py
@@ -3,3 +3,6 @@ from activation import sigmoid_func, relu_func, leaky_relu, tanh_func
 input_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
 print(f"Sigmoid Values: {[sigmoid_func(x) for x in input_values]}")
+print(f"ReLU Values: {[relu_func(x) for x in input_values]}")
+print(f"Leaky ReLU Values: {[leaky_relu(x) for x in input_values]}")
+print(f"Tanh Values: {[leaky_relu(x) for x in input_values]}")
